a4980b4a97949a238ab5dbdecec5fb943c08bdab
Soham407 <sohambhutkar1@gmail.com>
2025-12-30 23:44:13 +0530

    Cleanup: Remove redundant files and unsed components

--- components/index.ts
+++ components/index.ts
@@ -3,4 +3,3 @@ export { PanicButton } from './PanicButton';
 export { SafeToSpend } from './SafeToSpend';
 export { ConfirmationModal } from './ConfirmationModal';
 export { AppShell } from './AppShell';
-export { Onboarding } from './Onboarding';
